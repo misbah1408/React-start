bfe9a52e112ded948d053f153e0666f41a1df8b4
Mohammed Misbah <misbahmohammed00008@gmail.com>
2024-01-20 14:21:25 +0530

    ep-02

--- App.js
+++ App.js
@@ -1,4 +1,22 @@
 import React from "react"; 
 import ReactDOM from "react-dom/client";
 
+// const Jsxheading= <h1>hello</h1>;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(Jsxheading)
+const Title = ()=>(
+    <h1>React JavaScript</h1>
+);
 
+const HeadingComp = ()=>(
+    <>
+    <Title/>
+    <h1 id="head">
+        hello world
+    </h1>
+    </>
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( <HeadingComp/>);
